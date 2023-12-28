d61fbcb490b15e1ac5c039af9fcab40996695ae3
Varun Doshi <doshivarun202@gmail.com>
2023-12-28 15:20:33 +0530

    upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Hero = () => {
+  const [userData, setUserData] = useState({
+    address: "0x00000000000000000000000000",
+    start_date: "00/00/0000",
+    end_date: "00/00/0000",
+  });
+
+  const [gasSpent, setGasSpent] = useState(0);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let res = await axios.post("https://gas-lookup.onrender.com/lookup", {
+        address: event.target.elements.address.value,
+        start_date: event.target.elements.startDate.value,
+        end_date: event.target.elements.endDate.value,
+      });
+      console.log(res);
+      setGasSpent(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
+  return (
+    <div id="main-body">
+      <h1>Enter account details</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="address">ETH address</label>
+          <input id="address" type="text" />
+        </div>
+        <div>
+          <label htmlFor="startDate">Start Date</label>
+          <input id="startDate" type="text" />
+        </div>
+        <div>
+          <label htmlFor="endDate">End Date</label>
+          <input id="endDate" type="text" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {/* <button type="button" onClick={callServer}>
+        Click
+      </button> */}
+      <p>{gasSpent}</p>
+    </div>
+  );
+};
+
+export default Hero;
